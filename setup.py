93902c488a9fd28466a871654e537a252444af15
kimichen2907@gmail.com <kimichen2907@gmail.com>
2023-10-25 22:27:57 +0800

    Tdrn

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name = "To-do-record notbook",                                                                               ",
+    version = "0.0.1",
+    author = "ditisrmel",
+    license='MIT license',
+    author_email="kimichen2907@gmail.com",
+    description="Make a to-do record notebook",
+    url="https://github.com/ditisrmel/Tdrb", 
+    python_requires='>=3.11.4'
+)

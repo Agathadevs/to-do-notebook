c619a5f712d71b8919cb7985f10650f577e9bbf9
kimichen2907@gmail.com <kimichen2907@gmail.com>
2023-10-27 17:52:33 +0800

    Tdrn

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name = "To-do-record notbook",                                                                               ",
+    name = "Tdrd",                                                                                                                              
     version = "0.0.1",
     author = "ditisrmel",
     license='MIT license',
     author_email="kimichen2907@gmail.com",
     description="Make a to-do record notebook",
-    url="https://github.com/ditisrmel/Tdrb", 
+    url="https://github.com/ditisrmel/Tdrn", 
     python_requires='>=3.11.4'
 )

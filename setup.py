110c762999b7167da67a75c75b57d9bd65d3977d
kimichen2907@gmail.com <kimichen2907@gmail.com>
2023-12-09 12:04:13 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 
 setuptools.setup(
-    name = "Tdrd",                                                                                                                              
+    name = "Tdrn",                                                                                                                              
     version = "0.0.1",
-    author = "ditisrmel",
+    author = "Cheng",
     license='MIT license',
     author_email="kimichen2907@gmail.com",
     description="Make a to-do record notebook",
